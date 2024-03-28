6772752a7103c3204d815c9cace3dc2c95ac9c6e
Joan Kabura <kaburajoan25@gmail.com>
2024-03-29 02:33:54 +0300

    feat:hosting on aws

--- backend/mlImplementation/app.py
+++ backend/mlImplementation/app.py
@@ -45,5 +45,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    # app.run(port=5000, debug=True)
-    app.run(host='0.0.0.0', port=10000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
